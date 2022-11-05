4b90cec468c37a060d4ec60bc05cd4721b237bc7
henriquebenjamim <henriiqqq@gmail.com>
2022-11-05 16:02:19 -0300

    reordering some payment entities

---
+++ PaymentContext.Domain/Entities/CreditCardPayment.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentContext.Domain.Entities
+{
+    public class CreditCardPayment : Payment
+    {
+        public CreditCardPayment(int cardHolderName, string cardNumber, 
+        string lastTransactionNumber, DateTime paidDate, DateTime expireDate, 
+        decimal total, decimal totalPaid, string payer, string document, 
+        string address, string email) 
+        : base(paidDate, expireDate, total, totalPaid, payer, document, address, email)
+        {
+            CardHolderName = cardHolderName;
+            CardNumber = cardNumber;
+            LastTransactionNumber = lastTransactionNumber;
+        }
+
+        public int CardHolderName { get; private set; }
+        public string CardNumber { get; private set; }
+        public string LastTransactionNumber { get; private set; }
+    }
+}
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities
+{
+    [TestClass]
+    public class StudentTests
+    {
+        [TestMethod]
+        public void AdicionaAssinatura()
+        {   
+            var subscription = new Subscription(null);
+            var student = new Student("Henrique","Benjamim","12345678912", "hbbh@gmail.com");
+            student.AddSubscription(subscription);
+        }
+    }
+}
