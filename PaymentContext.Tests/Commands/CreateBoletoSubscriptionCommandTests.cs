cceca34956cfd7e14359e6c72cd4ad2f533a0f1f
henriquebenjamim <henriiqqq@gmail.com>
2022-11-09 20:10:46 -0300

    Added one simple test - validate name

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+
+            command.Validate();
+            Assert.AreEqual(false, command.Valid);
+        }
+    }
+}
